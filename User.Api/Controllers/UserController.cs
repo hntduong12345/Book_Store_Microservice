13d7340801b110d682f9a6b7bb5b6168895781ba
hntduong12345 <115139619+hntduong12345@users.noreply.github.com>
2026-01-21 19:37:08 +0700

    feat: add user controller functions

--- User.Api/Controllers/UserController.cs
+++ User.Api/Controllers/UserController.cs
@@ -10,17 +10,38 @@ namespace Users.Api.Controllers
     public class UserController(IUserService userService) : ControllerBase
     {
         [HttpGet("{id:guid}")]
-        public async Task<IActionResult> GetUser(Guid id)
+        public async Task<IActionResult> GetUserById([FromRoute]Guid id)
         {
             var user = await userService.GetUserByIdAsync(id);
             return user is not null ? Ok(user) : NotFound();
         }
 
+        [HttpGet]
+        public async Task<IActionResult> GetUserByEmail([FromQuery]string email)
+        {
+            var user = await userService.GetUserByEmailAsync(email);
+            return user is not null ? Ok(user) : NotFound();
+        }
+
         [HttpPost]
-        public async Task<IActionResult> CreateUser(CreateUserRequest request)
+        public async Task<IActionResult> CreateUser([FromBody]CreateUserRequest request)
         {
             await userService.CreateUserAsync(request);
             return Created();
+        }
+
+        [HttpPatch("{id:guid}")]
+        public async Task<IActionResult> UpdateUser([FromRoute] Guid id, [FromBody] UpdateUserRequest request)
+        {
+            await userService.UpdateUserAsync(id, request);
+            return NoContent();
+        }
+
+        [HttpPatch("{id:guid}/archived")]
+        public async Task<IActionResult> ArchivedUser([FromRoute] Guid id)
+        {
+            await userService.ArchivedUserAsync(id);
+            return NoContent();
         }
     }
 }
